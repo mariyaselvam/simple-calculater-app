a9d0f1bf055b77c3ea12709b996711a43c6b0ed7
mariyaselvam <anthuvan47@gmail.com>
2024-01-28 12:09:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+//    take the input value and store in to "outPutScreen" //
+const outPutScreen = document.getElementById("input")
+
+
+// take the input and display the input to clint //
+function display(num){
+    outPutScreen.value += num;
+}
+
+// clear the input feild //
+function clr(){
+   outPutScreen.value = "";
+}
+
+// calculate the input value and if it is any err show the err //
+function calculate(){
+    try{
+        outPutScreen.value = eval(outPutScreen.value);
+    }
+    catch{
+        alert("Invalid");
+    }
+};
+
+// clear input value one by one 
+function del(){
+        outPutScreen.value = outPutScreen.value.slice(0,-1)
+}
+
